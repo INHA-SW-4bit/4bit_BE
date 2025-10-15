dd912217010844727cbdf64ad1a0a4dd3bd648ef
omg <alsrl3156@gmail.com>
2025-10-15 15:32:11 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/org/example/nextchallenge/NextChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.example.nextchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nextchallenge/NextChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nextchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
